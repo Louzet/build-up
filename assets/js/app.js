61f41c78a322b21ca4642504d804fc92f0c976f3
Louzet <micklouzet@hotmail.fr>
2020-03-29 13:50:27 +0200

    new navigation with tailwind

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,40 @@
-import React, { Component, useState } from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, HashRouter, Route, Switch, withRouter, matchPath } from 'react-router-dom';
-
+import React, { Component, useState } from 'react'
+import ReactDOM from 'react-dom'
+import {
+	BrowserRouter as Router,
+	HashRouter,
+	Route,
+	Switch,
+	withRouter,
+	matchPath,
+} from 'react-router-dom'
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../css/app.css';
+import '../css/app.css'
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-import Home from "./pages/Home";
-import Registration from "./pages/auth/RegistrationPage";
-import Login from "./pages/auth/LoginPage";
-import Navbar from "./components/Navbar";
+import Home from './pages/Home'
+import Registration from './pages/auth/RegistrationPage'
+import Login from './pages/auth/LoginPage'
+import Navbar from './components/Navbar'
 
 const App = (props) => {
-    
-    
-    return (
-        <div className="app">
-            <Router>
-                <Navbar />
-                <div className="container-full">
-                    <Switch>
-                        <Route exact path="/login" component={Login} />
-                        <Route exact path="/registration" component={Registration} />
-                        <Route exact path="/" component={Home} />
-                    </Switch>
-                </div>
-            </Router> 
-        </div>
-    )
+	return (
+		<div className="app">
+			<Router>
+				<Navbar />
+				<div className="container-full">
+					<Switch>
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/registration" component={Registration} />
+						<Route exact path="/" component={Home} />
+					</Switch>
+				</div>
+			</Router>
+		</div>
+	)
 }
 
-export default App;
+export default App
 
-ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'))
